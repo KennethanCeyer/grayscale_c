f2a010ca4b97efe6dffbf8708318adb15e6a8b5e
KennethanCeyer <kenneth@pigno.se>
2021-05-19 03:38:51 +0900

    feat: initial commit :tada:

---
+++ math.c
@@ -0,0 +1,62 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <stdio.h>
+
+#define API extern __declspec(dllexport)
+
+API PyObject* sum(PyObject* pList) {
+    PyObject* pListItem;
+    double result = 0;
+    int length;
+    int i;
+
+    if (!PyList_Check(pList)) {
+        PyErr_SetString(PyExc_TypeError, "parameter must be a list.");
+        return NULL;
+    }
+
+    length = PyList_Size(pList);
+    for (i=0; i<length; ++i) {
+        pListItem = PyList_GetItem(pList, (Py_ssize_t)i);
+        result += PyFloat_AsDouble(pListItem);
+    }
+
+    return PyFloat_FromDouble(result);
+}
+
+API PyObject* mean(PyObject* pList) {
+    PyObject* pSum;
+    int length;
+
+    pSum = sum(pList);
+    length = PyList_Size(pList);
+
+    return PyFloat_FromDouble(PyFloat_AsDouble(pSum) / length);
+}
+
+API PyObject* var(PyObject* pList) {
+    PyObject* pListItem;
+    double result = 0;
+    double meanValue;
+    int length;
+    int i;
+
+    if (!PyList_Check(pList)) {
+        PyErr_SetString(PyExc_TypeError, "parameter must be a list.");
+        return NULL;
+    }
+
+    meanValue = PyFloat_AsDouble(mean(pList));
+    length = PyList_Size(pList);
+    for (i=0; i<length; ++i) {
+        pListItem = PyList_GetItem(pList, (Py_ssize_t)i);
+        result += pow(PyFloat_AsDouble(pListItem) - meanValue, 2);
+    }
+    result /= length;
+
+    return PyFloat_FromDouble(result);
+}
+
+API PyObject* std(PyObject* pList) {
+    return PyFloat_FromDouble(sqrt(PyFloat_AsDouble(var(pList))));
+}
---
+++ math.h
@@ -0,0 +1,6 @@
+#ifndef GRAYSCALE_MATH_H
+#define GRAYSCALE_MATH_H
+
+void hello(void);
+
+#endif //GRAYSCALE_MATH_H
