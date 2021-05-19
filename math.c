76f161ec1c4897816a8f3ccc264e12715200a0ae
KennethanCeyer <kenneth@pigno.se>
2021-05-20 05:09:42 +0900

    feat: add pow, sqrt, min, max functions

--- math.c
+++ math.c
@@ -8,7 +8,7 @@
     #define API
 #endif
 
-API PyObject* sum(PyObject* pList) {
+API PyObject* gs_sum(PyObject* pList) {
     PyObject* pListItem;
     double result = 0;
     int length;
@@ -28,17 +28,28 @@ API PyObject* sum(PyObject* pList) {
     return PyFloat_FromDouble(result);
 }
 
-API PyObject* mean(PyObject* pList) {
+API PyObject* gs_mean(PyObject* pList) {
     PyObject* pSum;
     int length;
 
-    pSum = sum(pList);
+    pSum = gs_sum(pList);
     length = PyList_Size(pList);
 
     return PyFloat_FromDouble(PyFloat_AsDouble(pSum) / length);
 }
 
-API PyObject* var(PyObject* pList) {
+API PyObject* gs_pow(PyObject* x, PyObject* y) {
+    double xValue = PyFloat_AsDouble(x);
+    double yValue = PyFloat_AsDouble(y);
+    return PyFloat_FromDouble(pow(xValue, yValue));
+}
+
+API PyObject* gs_sqrt(PyObject* x) {
+    double xValue = PyFloat_AsDouble(x);
+    return PyFloat_FromDouble(sqrt(xValue));
+}
+
+API PyObject* gs_var(PyObject* pList) {
     PyObject* pListItem;
     double result = 0;
     double meanValue;
@@ -50,7 +61,7 @@ API PyObject* var(PyObject* pList) {
         return NULL;
     }
 
-    meanValue = PyFloat_AsDouble(mean(pList));
+    meanValue = PyFloat_AsDouble(gs_mean(pList));
     length = PyList_Size(pList);
     for (i=0; i<length; ++i) {
         pListItem = PyList_GetItem(pList, (Py_ssize_t)i);
@@ -61,6 +72,60 @@ API PyObject* var(PyObject* pList) {
     return PyFloat_FromDouble(result);
 }
 
-API PyObject* std(PyObject* pList) {
-    return PyFloat_FromDouble(sqrt(PyFloat_AsDouble(var(pList))));
+API PyObject* gs_std(PyObject* pList) {
+    return PyFloat_FromDouble(sqrt(PyFloat_AsDouble(gs_var(pList))));
+}
+
+API PyObject* gs_min(PyObject* pList) {
+        PyObject* pListItem;
+        double result;
+        int i = 0;
+        int length;
+
+        if (!PyList_Check(pList)) {
+            PyErr_SetString(PyExc_TypeError, "parameter must be a list.");
+            return NULL;
+        }
+
+        length = PyList_Size(pList);
+        if (length == 0) {
+            PyErr_SetString(PyExc_ValueError, "list should not be empty.");
+            return NULL;
+        }
+
+        result = PyFloat_AsDouble(PyList_GetItem(pList, (Py_ssize_t)0));
+
+        for (i=1; i<length; ++i) {
+            pListItem = PyList_GetItem(pList, (Py_ssize_t)i);
+            result = min(result, PyFloat_AsDouble(pListItem));
+        }
+
+        return PyFloat_FromDouble(result);
+}
+
+API PyObject* gs_max(PyObject* pList) {
+        PyObject* pListItem;
+        double result;
+        int i = 0;
+        int length;
+
+        if (!PyList_Check(pList)) {
+            PyErr_SetString(PyExc_TypeError, "parameter must be a list.");
+            return NULL;
+        }
+
+        length = PyList_Size(pList);
+        if (length == 0) {
+            PyErr_SetString(PyExc_ValueError, "list should not be empty.");
+            return NULL;
+        }
+
+        result = PyFloat_AsDouble(PyList_GetItem(pList, (Py_ssize_t)0));
+
+        for (i=1; i<length; ++i) {
+            pListItem = PyList_GetItem(pList, (Py_ssize_t)i);
+            result = max(result, PyFloat_AsDouble(pListItem));
+        }
+
+        return PyFloat_FromDouble(result);
 }
--- math.h
+++ math.h
@@ -1,6 +1,4 @@
 #ifndef GRAYSCALE_MATH_H
 #define GRAYSCALE_MATH_H
 
-void hello(void);
-
 #endif //GRAYSCALE_MATH_H
